5d9742a8d9988c52f20bf54299629fb159ad371a
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-04-15 22:26:37 +0300

    Update rudy

--- login/index.php
+++ login/index.php
@@ -16,7 +16,10 @@ if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
     <style type="text/css">
         body{ font: 14px sans-serif; text-align: center; }
     </style>
---
+++ static_views/index.static.views.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Static Views Index</title>
+  </head>
+  <body>
+    <?php
+      require_once ("../databases/mysql/setupSystemDatabase.php");
+      require_once ("../databases/mysql/setupSystemTables.php");
+    ?>
+    <div class="container">
+      <div class="col-lg-12">
+        <h1>Static Views Index</h1>
+      </div>
+    </div>
+  </body>
+</html>
