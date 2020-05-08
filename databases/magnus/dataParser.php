1f8db950754ba52c96a52389f2a9eed769ada780
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-09 00:03:59 +0300

    Update dataParser.php

--- databases/magnus/dataParser.php
+++ databases/magnus/dataParser.php
@@ -1,3 +1,12 @@
 <?php
+	
+	$Data = fopen ("data/programming_languages_classification_json.data", "r") or die ("Unable to open file");
+
+	while(!feof($Data)) {
+
+	  echo(fgets($Data) . "<br>");
+	}
+
+	fclose($Data);
 
 ?>
